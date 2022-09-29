2c83e95425380ce56dfaa142202fa2b640301758
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-29 08:19:50 +0300

    commit tree d5aa2887f26b8cf0fbe997ef5a059270e17519ad

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+    
+    validate :title_does_not_contain?
+
+    # custom validation
+    # =================
+    def title_does_not_contain?
+        # buzzwords = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        # unless title.match? Regexp.union(buzzwords) 
+        buzzwords = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+        unless buzzwords.any? { |substr| substr.match title }
+        # unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top [number]") || title.include?("Guess")
+            errors.add(:title, "Title needs to be sufficiently clickbait-y!")
+        end
+    end
 end
